d964b1154656a80ddd36f808e0172959fd92b6cc
AntonA22 <antonabaluev2004@bk.ru>
2025-11-06 20:27:53 +0300

    supabase

--- Cafe/Presentation/LoginViewController.swift
+++ Cafe/Presentation/LoginViewController.swift
@@ -1,6 +1,33 @@
 import UIKit
+import Supabase
 
+struct LoginRequest: Codable {
+    let id: Int
+    let username: String
+    let password: String
+}
 
+struct User: Decodable {
+    let id: Int
+    let username: String
+    let password: String
+}
+
+import Foundation
+import Supabase
+
+final class SupabaseService {
+    static let shared = SupabaseService()
+    
+    let client: SupabaseClient
+    
+    private init() {
+        client = SupabaseClient(
+            supabaseURL: URL(string: "https://ahasxpolxjysjrcvulgw.supabase.co")!,
+            supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoYXN4cG9seGp5c2pyY3Z1bGd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNTMzMjUsImV4cCI6MjA3NzkyOTMyNX0.hGakP8HMHj3BgfS-lpM0kz1xZ8QV26tD4KjWSYo4gfI"
+        )
+    }
+}
 
 final class LoginViewController: UIViewController {
     
@@ -11,6 +38,65 @@ final class LoginViewController: UIViewController {
     
     // MARK: - IBAction
     @IBAction func loginClick(_ sender: Any) {
+        guard let username = loginInput.text, !username.isEmpty,
+              let password = passInput.text, !password.isEmpty else {
+                print("Поля пустые")
+                return
+        }
+            
+        let supabase = SupabaseService.shared.client
+            
+        Task {
+            do {
+                let supabase = SupabaseService.shared.client
+                
+                let response = try await supabase
+                    .from("users_swift")
+                    .select()
+                    .eq("username", value: username)
+                    .execute()
+                
+                let users = try JSONDecoder().decode([User].self, from: response.data)
+
+                guard let user = users.first else {
+                    print("Пользователь не найден")
+                    return
+                }
+
+                if user.password == password {
+                    print("Успешный вход!")
+                } else {
+                    print("Неверный пароль")
+                }
+                
+            } catch {
+                print("Ошибка supabase:", error)
+            }
+        }
+        
+        
+//            guard let email = loginInput.text, !email.isEmpty,
+//                  let password = passInput.text, !password.isEmpty else {
+//                print("Поля пустые")
+//                return
+//            }
+//
+//            let url = URL(string: "https://cafe.com/api/login")!
+//            var request = URLRequest(url: url)
+//            request.httpMethod = "POST"
+//            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//
+//            let body = LoginRequest(email: email, password: password)
+//            request.httpBody = try? JSONEncoder().encode(body)
+//
+//            URLSession.shared.dataTask(with: request) { data, response, error in
+//
+//                if let error = error {
+//                    print("Ошибка сети:", error)
+//                    return
+//                }
+//
+//            }.resume()
     }
     
     // MARK: - Lifecycle
@@ -18,6 +104,3 @@ final class LoginViewController: UIViewController {
         super.viewDidLoad()
     }
 }
-
-
-
--- Cafe/Presentation/RegistrationViewController.swift
+++ Cafe/Presentation/RegistrationViewController.swift
@@ -2,6 +2,21 @@ import UIKit
 
 
 
+struct RegistrationRequest: Codable {
+    let login: String
+    let email: String
+    let name: String
+    let password1: String
+    let password2: String
+}
+
+struct UserInsert: Codable {
+    let username: String
+    let email: String
+    let name: String
+    let password: String
+}
+
 final class RegistrationViewController: UIViewController {
     
     // MARK: - IBOutlet
@@ -13,6 +28,65 @@ final class RegistrationViewController: UIViewController {
     
     // MARK: - IBAction
     @IBAction func registrationClick(_ sender: Any) {
+        guard let login = loginInput.text, !login.isEmpty,
+                  let email = email_input.text, !email.isEmpty,
+                  let name = nameInput.text, !name.isEmpty,
+                  let p1 = passInput1.text, !p1.isEmpty,
+                  let p2 = passInput2.text, !p2.isEmpty else {
+                print("Поля пустые")
+                return
+            }
+
+        if p1 != p2 {
+            print("Пароли не совпадают")
+            return
+        }
+
+        let supabase = SupabaseService.shared.client
+        let body = UserInsert(
+            username: login,
+            email: email,
+            name: name,
+            password: p1
+        )
+        Task {
+            do {
+                let response = try await supabase
+                    .from("users_swift")
+                    .insert(body)
+                    .execute()
+
+                print("Пользователь создан", response)
+
+            } catch {
+                print("Ошибка при создании:", error)
+            }
+        }
+//            guard let login = loginInput.text, !login.isEmpty,
+//                  let email = email_input.text, !email.isEmpty,
+//                  let name = nameInput.text, !name.isEmpty,
+//                  let p1 = passInput1.text, !p1.isEmpty,
+//                  let p2 = passInput2.text, !p2.isEmpty else {
+//                print("Поля пустые")
+//                return
+//            }
+//            
+//            let body = RegistrationRequest(login: login, email: email, name: name, password1: p1, password2: p2)
+//            
+//            let url = URL(string: "https://cafe.com/api/register")!
+//            var request = URLRequest(url: url)
+//            request.httpMethod = "POST"
+//            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//            request.httpBody = try? JSONEncoder().encode(body)
+//            
+//            URLSession.shared.dataTask(with: request) { data, response, error in
+//                
+//                if let error = error {
+//                    print("Ошибка сети:", error)
+//                    return
+//                }
+//                
+//            }.resume()
     }
     
     // MARK: - Lifecycle
